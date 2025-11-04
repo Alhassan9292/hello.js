878d75f8b52a5826836e65ed4e9b624ff30a6acd
Alhassan Salifu <alhassansalifu468@gmail.com>
2025-11-04 22:37:50 +0100

    Initial commit: My fisrt week1 project work

---
+++ intro.js
@@ -0,0 +1,5 @@
+const name = "Salifu Alhassan";
+const age = 22;
+const favoriteProgrammingLanguage = "Python and JavaScript";
+
+console.log("Name: " + name + ", Age: " + age + ", Favorite Programming Language: " + favoriteProgrammingLanguage);
